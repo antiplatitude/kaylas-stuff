9d179d264f408d472755af9884fe885fc8b35455
Elijah Burke <ehburke@email.sc.edu>
2022-05-04 16:22:41 -0400

    Finished setup
    - Set gradle.properties
    - Set fabric.mod.json
    - Set directory structure
    - Defined code style and reformatted KaylasStuff.java to match

---
+++ src/main/java/com/antiplatitude/kaylas/stuff/KaylasStuff.java
@@ -0,0 +1,14 @@
+package com.antiplatitude.kaylas.stuff;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KaylasStuff implements ModInitializer
+{
+    public static final String MOD_ID = "kaylasstuff";
+
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
